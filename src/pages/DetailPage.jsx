b45aaa59e833dbf7883f3d74f8b644e7862aae32
RekhaRavi34 <rekharavi170397@gmail.com>
2024-05-11 17:19:47 +0530

    final commit

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Breadcrumb } from 'antd';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
